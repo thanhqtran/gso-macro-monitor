4b3598087afc4c0223afe4cb5a805afe451b96e9
Quang-Thanh Tran (tedd) <66583019+thanhqtran@users.noreply.github.com>
2024-11-20 19:00:04 +0900

    Update scrape_all.py

--- scrape_all.py
+++ scrape_all.py
@@ -2,11 +2,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import xmltodict, json
-import matplotlib.pyplot as plt
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import imageio.v2 as imageio
 import textwrap
 
 # read database
